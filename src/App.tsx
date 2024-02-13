6f35b97e3de38ff99cfda8f0e827e8192466e5c7
Sarun6618 <ch.sarun0904@gmail.com>
2024-02-13 08:05:52 +0530

    try 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
 import React from "react";
 import "./App.css";
-import MainRouter from "./routes";
+// import Sidebar from "./components/sidebar/sidebar";
+// import { Route, Routes } from "react-router-dom";
+// import MainRouter from "./routes";
+// import Profile from "./components/profile/Profile";
+import Signup from "./pages/Signup";
+// import MainRouter from "./routes";
 // import Signup from "./pages/Signup";
 // import Login from "./pages/Login";
 function App() {
+  // const [selectedItem, setSelectedItem] = useState(null);
+
+  // const handleSidebarItemClick = (item: any) => {
+  //   setSelectedItem(item);
+  // };
   return (
     <>
-      <MainRouter />
-      {/* <Signup /> */}
-      {/* <Login /> */}
+      {/* <div className="app-container">
+        <div className="top-bar">
+          <div className="profileIcon absolute">
+            <Profile />
+          </div>
+        </div>
+        <div className="main">
+          <div className="side-bar">
+            <Sidebar />
+          </div>
+          <div className="context-container">
+            <MainRouter />
+          </div>
+        </div>
+      </div> */}
+      <Signup />
     </>
   );
 }
--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,18 +1,9 @@
 import React from "react";
-import Sidebar from "../components/sidebar/sidebar";
-import Profile from "../components/profile/Profile";
-import Dropdown from "../components/dropdown/Dropdown";
 
 const Dashboard = () => {
   return (
     <>
-      <div className="sidebar">
-        <Sidebar />
-      </div>
-      <div className="absolute top-5 right-10 p-4">
-        <Profile />
-      </div>
-      {/* <Dropdown /> */}
+      <h1>Dashboard</h1>
     </>
   );
 };
