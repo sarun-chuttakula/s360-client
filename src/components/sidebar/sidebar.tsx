1701bb68fbf53ef240faac7c3488d46a1950a71a
Sarun6618 <ch.sarun0904@gmail.com>
2024-02-24 13:32:44 +0530

    change sidebar color

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 import { RxDashboard } from "react-icons/rx";
 import { LuMessagesSquare } from "react-icons/lu";
 import { GiCash } from "react-icons/gi";
@@ -9,34 +9,75 @@ import { IoLibraryOutline } from "react-icons/io5";
 import useAuth from "../../hooks/useAuth";
 
 const Sidebar = () => {
+  const location = useLocation();
+  const [activeItem, setActiveItem] = useState(
+    localStorage.getItem("activeItem") || ""
+  );
+
+  // Set the active item based on the current pathname
+  useEffect(() => {
+    setActiveItem(localStorage.getItem("activeItem") || "");
+  }, []);
+
+  // Update the active item when a link is clicked
+  const handleItemClick = (path: any) => {
+    setActiveItem(path);
+    localStorage.setItem("activeItem", path);
+  };
+
   const auth = useAuth();
+
   return (
     <div className="side-bar">
-      <Link to="/">
-        <RxDashboard />
-        <span>Dashboard</span>
+      <Link
+        to="/"
+        onClick={() => handleItemClick("/")}
+        className={activeItem === "/" ? "active" : ""}
+      >
+        <RxDashboard className="sidebar-icon" />
+        <span className="sidebar-text">Dashboard</span>
       </Link>
-      <Link to="/groups">
-        <LuMessagesSquare />
-        <span>Groups</span>
+      <Link
+        to="/groups"
+        onClick={() => handleItemClick("/groups")}
+        className={activeItem === "/groups" ? "active" : ""}
+      >
+        <LuMessagesSquare className="sidebar-icon" />
+        <span className="sidebar-text">Groups</span>
       </Link>
-      <Link to="/fee-details">
-        <GiCash />
-        <span>Financial Details</span>
+      <Link
+        to="/fee-details"
+        onClick={() => handleItemClick("/fee-details")}
+        className={activeItem === "/fee-details" ? "active" : ""}
+      >
+        <GiCash className="sidebar-icon" />
+        <span className="sidebar-text">Financial Details</span>
       </Link>
-      <Link to="/result-board">
-        <GiRank3 />
-        <span>ResultBoard</span>
+      <Link
+        to="/result-board"
+        onClick={() => handleItemClick("/result-board")}
+        className={activeItem === "/result-board" ? "active" : ""}
+      >
+        <GiRank3 className="sidebar-icon" />
+        <span className="sidebar-text">ResultBoard</span>
       </Link>
-      {auth?.role === "teacher" ? (
-        <Link to="/attendance-board">
-          <AiFillCarryOut />
-          <span>AttendanceBoard</span>
+      {auth?.role === "teacher" && (
+        <Link
+          to="/attendance-board"
+          onClick={() => handleItemClick("/attendance-board")}
+          className={activeItem === "/attendance-board" ? "active" : ""}
+        >
+          <AiFillCarryOut className="sidebar-icon" />
+          <span className="sidebar-text">AttendanceBoard</span>
         </Link>
-      ) : null}
-      <Link to="/library">
-        <IoLibraryOutline />
-        <span>library</span>
+      )}
+      <Link
+        to="/library"
+        onClick={() => handleItemClick("/library")}
+        className={activeItem === "/library" ? "active" : ""}
+      >
+        <IoLibraryOutline className="sidebar-icon" />
+        <span className="sidebar-text">library</span>
       </Link>
     </div>
   );
